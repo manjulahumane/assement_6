cc6389c05b60e5920f67e381dd0edf049da01f35
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 19:08:15 +0530

    upload file

---
+++ Per.java
@@ -0,0 +1,16 @@
+class Per{
+static float[]percentage={1.11f,2.1f,3.1f,4.1f,5.3f,6.4f,7.3f,8.4f,9.8f,1.13f};
+public static void main(String[] args){
+System.out.println(percentage[0]);
+System.out.println(percentage[1]);
+System.out.println(percentage[3]);
+System.out.println(percentage[4]);
+System.out.println(percentage[5]);
+System.out.println(percentage[6]);
+System.out.println(percentage[7]);
+System.out.println(percentage[8]);
+System.out.println(percentage[9]);
+
+}
+
+}
